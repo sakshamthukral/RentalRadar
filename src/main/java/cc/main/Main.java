b5a062ab92aaae2b15138b29e417f11391dee814
sakshamthukral <thukral.saksham132@gmail.com>
2023-11-21 21:59:58 -0500

    changes in AutoComplete, Main, crawlLiv, config

--- src/main/java/cc/main/Main.java
+++ src/main/java/cc/main/Main.java
@@ -1,6 +1,7 @@
 package cc.main;
 import cc.crawler.*;
 import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,16 +11,15 @@ import cc.Autocomplete.Trie;
 
 import java.io.*;
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import cc.Autocomplete.AutoComplete;
+
 import org.apache.commons.io.FileUtils;
 import cc.utils.config;
 public class Main {
     public static final String chromeDriverPath = config.chromeDriverPath;
     public static final String chromeBrowserPath = config.chromeBrowserPath;
+    public static String globCity = "";
 
     public static void createFolderIfNotExists(String folderPath){
         File folder = new File(folderPath);
@@ -35,20 +35,47 @@ public class Main {
     }
 
     // Function to save the last execution time
-    public static void saveLastRunTime(String lastRunTimeFilePath) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(lastRunTimeFilePath))) {
+//    public static void saveLastRunTime(String lastRunTimeFilePath) {
+//        try (PrintWriter writer = new PrintWriter(new FileWriter(lastRunTimeFilePath))) {
+//            long currentTime = System.currentTimeMillis();
+//            writer.println(currentTime);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    // Function to get the last execution time
+//    public static long getLastRunTime(String lastRunTimeFilePath) {
+//        try (BufferedReader reader = new BufferedReader(new FileReader(lastRunTimeFilePath))) {
+//            String line = reader.readLine();
+//            return line != null ? Long.parseLong(line) : 0;
+//        } catch (IOException | NumberFormatException e) {
+//            return 0;
+//        }
+//    }
+    public static void saveLastRunTime(String lastRunTimeFilePath, String cityName) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(lastRunTimeFilePath, true))) {
             long currentTime = System.currentTimeMillis();
-            writer.println(currentTime);
+            writer.println(cityName + ":" + currentTime);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    // Function to get the last execution time
-    public static long getLastRunTime(String lastRunTimeFilePath) {
+    // Function to get the last execution time for a specific city
+    public static long getLastRunTime(String lastRunTimeFilePath, String cityName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(lastRunTimeFilePath))) {
-            String line = reader.readLine();
-            return line != null ? Long.parseLong(line) : 0;
+            Map<String, Long> cityTimestamps = new HashMap<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String city = parts[0];
+                    long timestamp = Long.parseLong(parts[1]);
+                    cityTimestamps.put(city, timestamp);
+                }
+            }
+            return cityTimestamps.getOrDefault(cityName, 0L);
         } catch (IOException | NumberFormatException e) {
             return 0;
         }
@@ -89,70 +116,36 @@ public class Main {
         } catch(Exception e){
             System.out.println(e);
         }
-                String[] cities = { "Toronto", "Montreal", "Calgary", "Ottawa", "Edmonton", "Winnipeg",
-                "Vancouver", "Brampton", "Hamilton", "Surrey", "Quebec", "Halifax", "Laval",
-                "London", "Markham", "Vaughan", "Gatineau", "Saskatoon", "Kitchener", "Longueuil",
-                "Burnaby", "Windsor", "Regina", "Oakville", "Richmond", "Richmond", "Burlington",
-                "Oshawa", "Sherbrooke", "Sudbury", "Abbotsford", "Coquitlam", "Barrie", "Saguenay",
-                "Kelowna", "Guelph", "Whitby", "Cambridge", "Catharines", "Milton", "Langley", "Kingston",
-                "Ajax", "Waterloo", "Terrebonne", "Saanich", "Delta", "Brantford", "Clarington", "Nanaimo",
-                "Strathcona", "Pickering", "Lethbridge", "Kamloops", "Richelieu", "Niagara", "Cape Breton",
-                "Chilliwack", "Victoria", "Brossard", "Newmarket", "Repentigny", "Peterborough",
-                "Moncton", "Drummondville", "Caledon", "Airdrie", "Sarnia", "Granby", "Fredericton",
-                "Aurora", "Mirabel", "Blainville", "Welland", "Belleville" };
-        Trie trie = new Trie();
-        // Added all major cities to the trie.
 
-        for (String city : cities) {
-            trie.insert(city);
-        }
-        trie.loadSearchFrequenciesFromFile("D:\\ACC\\compute-champions\\src\\main\\java\\cc\\SearchFrequency\\searchFrequencies.txt");
 
         System.out.print("Enter the city where you are looking for rentals: ");
         String city = sc.nextLine();
-                    // Check if the input contains only alphabetic characters
-                if (!city.matches("^[a-zA-Z]+$")) {
-                    System.out.println("Invalid input.1 Please enter a valid city name.");
-                }else{
-                List<String> suggestions = trie.autoComplete(city);
-                System.out.println("Autocomplete suggestions:");
-                String suggestion="";
-                for (int i = 0; i < suggestions.size(); i++) {
-                    suggestion = suggestions.get(i);
-                    System.out.println((i + 1) + ". " + suggestion);
-                }
-                 int selectedOption=0;
-                if (suggestions.size()>1) {
-                    System.out.print("Enter the number for the suggestion you want to select: ");
-                   
-                    try {
-                        selectedOption = sc.nextInt();
-                        sc.nextLine(); // Consume the newline character1
-                    } catch (InputMismatchException e) {
-                        System.out.println("Invalid input. Try again!");
-                        sc.nextLine(); // Consume the invalid input
-    
-                    }
+        while(!config.CITIES.contains(city)) {
+            while (Objects.equals(city, "") || (!city.matches("^[a-zA-Z]+$"))) {
+                System.out.print("Invalid City please renter the city: ");
+                city = sc.nextLine();
+            }
+            String suggestion = "";
+            if (city.length() < config.minCityLength) {
+                AutoComplete ac = new AutoComplete();
+                suggestion = ac.runAutoComplete(city);
+            }
+            if (city.length() >= config.minCityLength || !config.CITIES.contains(suggestion)) {
+                System.out.println("SpellChecker suggestions:");
+//                suggestion="windsor";
+//            SpellChecker sc = new SpellChecker();
+//            city=sc.runSpellChecker(city);
+            }
+            System.out.println(suggestion);
+            city = suggestion;
 
-                            if (selectedOption > 0 && selectedOption <= suggestions.size()) {
-                                suggestion = suggestions.get(selectedOption - 1);
-                                System.out.println("City Name " + suggestion + " is selected");
-                                trie.displaySearchFrequency(suggestion); // Display search frequency for the selected city
-                                trie.incrementSearchFrequency(suggestion); // Increment the search frequency for the selected city
-                            } else {
-                                System.out.println("Invalid selection. Try Again!");
-                            }
-                        } else 
-                        {
-                        	for (int i = 0; i < suggestions.size(); i++) 
-       			    	   {
-       					        suggestion = suggestions.get(i);
-       					        System.out.println("City Name "+ suggestion+ " is selected");
-       					     trie.displaySearchFrequency(suggestion);
-       					     trie.incrementSearchFrequency(suggestion);
-       					   }
-                        }
-                    
+        }
+
+        long lastRunTime = getLastRunTime(lastRunTimeFilePath,city.toLowerCase());
+        System.out.println("Last run time for " + city + ": " + (lastRunTime > 0 ? new Date(lastRunTime) : "N/A"));
+        System.out.print("Do you want to rerun the program? (y/n): ");
+        String rerunChoice = sc.nextLine().toLowerCase();
+        if ("y".equals(rerunChoice)) {
 
 
         System.out.print("Enter the number of pages(<5 in order to prevent longer running times) you want to scrape: ");
@@ -172,19 +165,19 @@ public class Main {
         createFolderIfNotExists(HTMLFolderPath);
         createFolderIfNotExists(txtFolderPath);
         if(websiteCode==1){
-            crawlLiv.driveCrawling(driver, wait, numPages, suggestion);
+            crawlLiv.driveCrawling(driver, wait, numPages, city);
             parseLiv.processHtmlFiles(HTMLFolderPath, txtFolderPath);
         }else if(websiteCode==2){
-            crawlRental.driveCrawling(driver, wait, numPages, suggestion);
+            crawlRental.driveCrawling(driver, wait, numPages, city);
             parseRental.processHtmlFiles(HTMLFolderPath, txtFolderPath);
-        } else if (websiteCode==3) {
-            crawlRentSeeker.driveCrawling(driver, wait, numPages, suggestion);
+        }else if (websiteCode==3) {
+            crawlRentSeeker.driveCrawling(driver, wait, numPages, city);
             parseRentSeeker.processHtmlFiles(HTMLFolderPath, txtFolderPath);
         }
-        saveLastRunTime(lastRunTimeFilePath);
-       }// else of  if (!city.matches("^[a-zA-Z]+$")) closes here
-       trie.saveSearchFrequenciesToFile("D:\\ACC\\compute-champions\\src\\main\\java\\cc\\SearchFrequency\\searchFrequencies.txt");
-    }
+        saveLastRunTime(lastRunTimeFilePath, city);
+        globCity = city;
+        }
+       }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while (true){
@@ -199,29 +192,23 @@ public class Main {
                 String lastRunTimeFilePath=paths.get(2);
                 int websiteCode=Integer.parseInt(paths.get(3));
 
-                long lastRunTime = getLastRunTime(lastRunTimeFilePath);
-                System.out.println("Last Execution Time: " + (lastRunTime > 0 ? new Date(lastRunTime) : "N/A"));
-                System.out.print("Do you want to rerun the program? (y/n): ");
-                String rerunChoice = sc.nextLine().toLowerCase();
-                if ("y".equals(rerunChoice)) {
-                    runCrawlingAndParsing(sc, HTMLFolderPath, txtFolderPath, lastRunTimeFilePath, websiteCode);
-                } else{
-                    // Following code is for inputting a text file and getting the filtered text after removal of all stopwords
-//                    try {
-//                        List<String> stopwords = readStopwords(config.stopwordsFilePath);
-//                        filterText(inputTextFilePath, outputFilePath, stopwords);
-//                        System.out.println("Stopwords removed successfully!");
-//                    } catch (IOException e) {
-//                        System.out.println("An error occurred: " + e.getMessage());
-//                    }
-                    System.out.println("Running the rest of the program!!");
-                }
-            } else if (webOption == 2) {
-                System.out.println("Initiated inverted indexing");
-            } else if (webOption == 3){
+                runCrawlingAndParsing(sc, HTMLFolderPath, txtFolderPath, lastRunTimeFilePath, websiteCode); // word compeletion -> spell check -> crawling -> parsing
+
+//                globCity="windsor";
+//                long lastRunTime = getLastRunTime(lastRunTimeFilePath,globCity);
+//                System.out.println(lastRunTime);
+//                System.out.println("Last Execution Time: " + (lastRunTime > 0 ? new Date(lastRunTime) : "N/A"));
+
+//                System.out.println("Last run time for " + globCity + ": " + lastRunTime);
+//                System.out.print("Do you want to rerun the program? (y/n): ");
+//                String rerunChoice = sc.nextLine().toLowerCase();
+//                if ("y".equals(rerunChoice)) {
+//                    runCrawlingAndParsing(sc, HTMLFolderPath, txtFolderPath, lastRunTimeFilePath, websiteCode);
+//                }
+
+            }
+            if(webOption == 3){
                 break;
-            } else {
-                System.out.println("Not a valid option!");
             }
         }
         
--- src/main/java/cc/utils/config.java
+++ src/main/java/cc/utils/config.java
@@ -1,5 +1,8 @@
 package cc.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class config {
     public static final String HTMLFolderPathLiv="crawled_liv.rent";
     public static final String HTMLFolderPathRental="crawled_rental.ca";
@@ -15,4 +18,31 @@ public class config {
    // public static final String chromeDriverPath = "C:/Users/aniru/Downloads/acc_assignment_3/chromedriver-win64/chromedriver-win64/chromedriver.exe";
    // public static final String chromeBrowserPath = "C:/Program Files/Google/Chrome/Application/chrome.exe";
     public static final String stopwordsFilePath = "stopwords.txt"; // Path to stopwords file
+
+    public static final String[] cities = {"Toronto", "Montreal", "Calgary", "Ottawa", "Edmonton", "Winnipeg",
+                "Vancouver", "Brampton", "Hamilton", "Surrey", "Quebec", "Halifax", "Laval",
+                "London", "Markham", "Vaughan", "Gatineau", "Saskatoon", "Kitchener", "Longueuil",
+                "Burnaby", "Windsor", "Regina", "Oakville", "Richmond", "Burlington",
+                "Oshawa", "Sherbrooke", "Sudbury", "Abbotsford", "Coquitlam", "Barrie", "Saguenay",
+                "Kelowna", "Guelph", "Whitby", "Cambridge", "Catharines", "Milton", "Langley", "Kingston",
+                "Ajax", "Waterloo", "Terrebonne", "Saanich", "Delta", "Brantford", "Clarington", "Nanaimo",
+                "Strathcona", "Pickering", "Lethbridge", "Kamloops", "Richelieu", "Niagara", "Cape Breton",
+                "Chilliwack", "Victoria", "Brossard", "Newmarket", "Repentigny", "Peterborough",
+                "Moncton", "Drummondville", "Caledon", "Airdrie", "Sarnia", "Granby", "Fredericton",
+                "Aurora", "Mirabel", "Blainville", "Welland", "Belleville"};
+    public static List<String> CITIES = Arrays.asList("toronto", "montreal", "calgary", "ottawa", "edmonton", "winnipeg",
+            "vancouver", "brampton", "hamilton", "surrey", "quebec", "halifax", "laval",
+            "london", "markham", "vaughan", "gatineau", "saskatoon", "kitchener", "longueuil",
+            "burnaby", "windsor", "regina", "oakville", "richmond", "burlington",
+            "oshawa", "sherbrooke", "sudbury", "abbotsford", "coquitlam", "barrie", "saguenay",
+            "kelowna", "guelph", "whitby", "cambridge", "catharines", "milton", "langley", "kingston",
+            "ajax", "waterloo", "terrebonne", "Saanich", "delta", "brantford", "clarington", "nanaimo",
+            "strathcona", "pickering", "lethbridge", "kamloops", "richelieu", "niagara", "cape Breton",
+            "chilliwack", "victoria", "brossard", "newmarket", "repentigny", "peterborough",
+            "moncton", "drummondville", "caledon", "airdrie", "sarnia", "granby", "fredericton",
+            "aurora", "mirabel", "blainville", "welland", "belleville");
+
+    public static final String searchFrequencyFilePath = "src/main/java/cc/SearchFrequency/searchFrequencies.txt";
+    public static final String cityRegex = "^[a-zA-Z]+$";
+    public static final int minCityLength = 4;
 }
